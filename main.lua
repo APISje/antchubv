c293d8035d1bbbd535c17678e0a2f84dacb34ca8
APISje <aapis3308@gmail.com>
2025-11-03 03:51:42 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,11 +3,11 @@
     | | /| / (_)__  ___/ / / / /  _/
     | |/ |/ / / _ \/ _  / /_/ // /  
     |__/|__/_/_//_/\_,_/\____/___/
-    
+
     v1.6.6  |  2025-11-02  |  Roblox UI Library for scripts
-    
+
     To view the source code, see the `src/` folder on the official GitHub repository.
-    
+
     Author: Footagesus (Footages, .ftgs, oftgs)
     Github: https://github.com/Footagesus/WindUI
     Discord: https://discord.gg/ftgs-development-hub-1300692552005189632
